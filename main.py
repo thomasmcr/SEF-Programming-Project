9bb1bef89b84f2317d315049b5010d331f928714
thomasmcr <thomas.mmcr2022@gmail.com>
2023-02-20 18:01:57 +0000

    Removed Tkinter from the project
    Was under the impression we could use a GUI library but Bruce clarified we should stick to printing to the command line

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-import tkinter
-from tkinter import *
-window = Tk()
-
-
 def main():
-    window.title("Hello World!")
-    window.geometry("1280x720")
-    create_table(3, 6)
-    window.mainloop()
-
-
-# Creates a table, loading the data from the datastore
-def create_table(width, height):
-    for x in range(width):
-        for y in range(height):
-            grid_frame = Frame(window, relief="solid")
-
-            entry = Entry(grid_frame, width=10, fg="blue", font=("Arial", 16, "bold"), justify=CENTER)
-            button = Button(grid_frame, width=5, fg="blue", font=("Arial", 16, "bold"), text="info")
-
-            entry.pack(side="left", fill="both", expand=True)
-            button.pack(side="right", fill="x")
-
-            grid_id_string = str(x) + ":" + str(y)
-            entry.insert(END, grid_id_string)
-            entry.config(state="readonly")
-
-            grid_frame.grid(row=y, column=x)
+    print("Hello World!")
 
 
 # Press the green button in the gutter to run the script.
