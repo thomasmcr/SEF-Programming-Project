a8cfd018cdbf3b8539470e7ca853d8c977aa3ad3
thomasmcr <thomas.mmcr2022@gmail.com>
2023-02-28 20:27:25 +0000

    Refactored code and added additional commands

---
+++ database_manager.py
@@ -0,0 +1,124 @@
+import os
+import sqlite3
+from sqlite3 import Error
+
+
+database_filename = "ticket_database"
+database_connection = sqlite3.Connection
+# I've built the validation rules into the table itself, data cannot be entered into the table if it is not of the
+# correct type and meets the conditions defined inside the CHECK() statement
+sql_create_tickets_table = """ 
+CREATE TABLE IF NOT EXISTS tickets (
+    id integer PRIMARY KEY NOT NULL UNIQUE,
+    software_name text NOT NULL CHECK(length(software_name) > 0),
+    description text NOT NULL CHECK(length(description) > 0),
+    email text NOT NULL CHECK(length(email) > 0), 
+    priority integer CHECK(priority <= 10 and priority >= 1) NOT NULL,
+    submit_date text NOT NULL,
+    resolved boolean NOT NULL,
+    resolution_comment text,
+    resolution_date text 
+); """
+
+
+
+def initialise():
+    # Directory of the project
+    directory = os.path.dirname(__file__)
+
+    # Directory of the database file
+    database_directory = os.path.join(directory, database_filename)
+
+    # Create the database connection
+    global database_connection
+    database_connection = create_database_connection(database_directory)
+
+    # Create the tickets table
+    create_database_table(sql_create_tickets_table)
+
+
+def close():
+    database_connection.close()
+
+
+def create_database_connection(database_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(database_file)
+        return conn
+        print("Successfully connected to database")
+    except Error as error:
+        print(error)
+
+
+def create_database_table(create_table_sql):
+    try:
+        c = database_connection.cursor()
+        c.execute(create_table_sql)
+        print("Successfully created table")
+    except Error as error:
+        print(error)
+
+
+def create_ticket(ticket):
+    sql = """INSERT INTO tickets(software_name,description,email,priority,submit_date,
+             resolved,resolution_comment,resolution_date)
+             VALUES(?,?,?,?,?,?,?,?) """
+    cur = database_connection.cursor()
+    cur.execute(sql, ticket)
+    database_connection.commit()
+
+
+def delete_ticket(ticket_id):
+    ticket_id = ticket_id.strip()
+    sql = """DELETE FROM tickets WHERE id ="""+ticket_id
+    cur = database_connection.cursor()
+    cur.execute(sql)
+    database_connection.commit()
+
+
+def amend_ticket(ticket_id, amended_ticket):
+    sql = """UPDATE tickets SET software_name = ?,
+    description = ?, email = ?, priority = ?, submit_date = ?, 
+    resolved = ?, resolution_comment = ?, resolution_date = ? WHERE id="""+ticket_id
+    cur = database_connection.cursor()
+    cur.execute(sql, amended_ticket)
+    database_connection.commit()
+
+
+def get_table():
+    cur = database_connection.cursor()
+    cur.execute("SELECT * FROM tickets")
+    # Can be replaced with the property name I'm trying to get
+    tickets = cur.fetchall()
+    return tickets
+
+
+def get_number_of_active_tickets():
+    cur = database_connection.cursor()
+    cur.execute("SELECT COUNT(*) FROM tickets")
+    return cur.fetchone()[0]
+
+
+def get_ticket(ticket_id):
+    ticket_id = ticket_id.strip()
+    cur = database_connection.cursor()
+    cur.execute("SELECT * FROM tickets WHERE id ="+ticket_id)
+    ticket = cur.fetchall()
+    return ticket
+
+
+# Checks the date in the tickets table is valid and isn't corrupt. should be called before reading or after writing to
+# the database
+def validate_table_contents():
+    for ticket in get_table():
+        ticket_id = ticket[0]
+        description = ticket[1]
+        priority = ticket[3]
+        submit_date = ticket[4]
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,73 +3,65 @@ from sqlite3 import Error
 import os
 import cmd
 from datetime import date
-
-# The name of the database file, this should be set manually
-database_filename = "ticket_database"
-# The path of the directory
-DIRNAME = ""
-# The path to the database file
-DATABASE_FILE_PATH = r""
-database_connection = sqlite3.Connection
-sql_create_tickets_table = """ 
-CREATE TABLE IF NOT EXISTS tickets (
-    id integer PRIMARY KEY,
-    name text NOT NULL,
-    priority integer,
-    submit_date text NOT NULL
-); """
-
-# The table needs to be expanded so that each entry has 8 columns
+import database_manager
+from database_manager import *
 
 
 def main():
-    # Store directory and database paths in constant variables
-    initialise_directory_variables()
+    # Connect to database
+    database_manager.initialise()
 
-    # Create conncetion to SQL database
-    global database_connection
-    database_connection = create_database_connection(DATABASE_FILE_PATH)
-
-    # Create a table in the database
-    create_database_table(sql_create_tickets_table)
-
-    # Test submission of a ticket to the database
-    # ticket_1 = ("Every time I open your tool it crashes my laptop!", 10, "2023-20-02")
-    # create_ticket(ticket_1)
+    # Start the CMD library mainloop
     MainLoop().cmdloop()
-    # Print tables contents to the console
-    database_connection.close()
+
+    # Close the database
+    database_manager.close()
 
 
 class MainLoop(cmd.Cmd):
-    intro = "Welcome to the ticket manager, type help or ? to list commands."
+    intro = "Welcome to the ticket manager, type help or ? to list commands. To create a new ticket simply type: create"
     prompt = ">>"
 
     def do_list(self, arg):
-        """get all the tickets"""
-        print_table()
+        """Prints ticket with matching ID to the console. If no ID is provided, all tickets are printed
+            >>list   | prints all tickets
+            >>list 1 | prints ticket with an ID of 1
+        """
 
-    def do_list_specific(self, arg):
-        """gets a specific ticket, enter the id of the ticket you want e.g. print_ticket 1"""
-        if not arg.isnumeric():
-            print("Argument cannot be string")
+        if get_number_of_active_tickets() <= 0:
+            print("There are no active tickets")
             return
-        if arg == "":
-            print("No argument provided")
+        elif arg == "":
+            for ticket in get_table():
+                print(ticket)
+        elif not arg.isnumeric():
+            print("Argument must be an integer ID")
             return
-        print_ticket(arg[0])
+        else:
+            print(get_ticket(arg[0]))
+
+    def do_exit(self, arg):
+        """Exits the application"""
+        print("Powering down :O")
+        return True
+
 
     def do_count(self, arg):
         """Gets the number of active tickets"""
-        print_number_of_active_tickets()
+        count = get_number_of_active_tickets()
+        print("There are " + str(count) + " active tickets")
 
     def do_create(self, arg):
         """Creates a ticket and adds it to the tickets table"""
-        description = input("Please describe your problem: ")
-        priority = int(input("Please enter a priority for your ticket between 1 and 10: "))
         current_date = date.today().strftime("%d/%m/%Y")
+        software_name = validate_string_input("Please enter the name of the software package the ticket relates to: ")
+        description = validate_string_input("Please describe your problem: ")
+        email = validate_string_input("Enter a contact email so we can get back to you: ")
+        priority = 5
+        submit_date = current_date
+        resolved = False
 
-        ticket = (description, priority, current_date)
+        ticket = (software_name, description, email, priority, submit_date, resolved, None, None)
         create_ticket(ticket)
 
     def do_delete(self, arg):
@@ -77,76 +69,41 @@ class MainLoop(cmd.Cmd):
         if not arg.isnumeric():
             print("Argument cannot be string")
             return
-        if arg == "":
+        elif arg == "":
             print("No argument provided")
             return
-        delete_ticket(arg[0])
+        else:
+            delete_ticket(arg[0])
+
+    def do_amend(self, arg):
+        """Amends ticket with the given ID
+            >>amend 1  | amends the details of ticket with ID 1
+        """
+        if arg == "":
+            print("Please provide a valid ticket ID")
+            return
+        elif not arg.isnumeric():
+            print("Argument must be an integer ID")
+            return
+
+        current_date = date.today().strftime("%d/%m/%Y")
+        software_name = validate_string_input("Enter the amended name of the software package the ticket relates to: ")
+        description = validate_string_input("Enter the amended problem description: ")
+        email = validate_string_input("Enter the amended contact email:  ")
+        priority = 5
+        submit_date = current_date
+        resolved = False
+        amended_ticket = (software_name, description, email, priority, submit_date, resolved, None, None)
+        database_manager.amend_ticket(arg[0], amended_ticket)
 
 
-def initialise_directory_variables():
-    global DIRNAME
-    DIRNAME = os.path.dirname(__file__)
-
-    global DATABASE_FILE_PATH
-    DATABASE_FILE_PATH = os.path.join(DIRNAME, database_filename)
-
-
-def create_database_connection(database_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(database_file)
-        print("Successfully connected to database")
-        return conn
-    except Error as error:
-        print(error)
-
-
-def create_database_table(create_table_sql):
-    try:
-        c = database_connection.cursor()
-        c.execute(create_table_sql)
-        print("Successfully created table")
-    except Error as error:
-        print(error)
-
-
-def create_ticket(ticket):
-    sql = """INSERT INTO tickets(name,priority,submit_date)
-                  VALUES(?,?,?) """
-    cur = database_connection.cursor()
-    cur.execute(sql, ticket)
-    database_connection.commit()
-    return cur.lastrowid
-
-
-def delete_ticket(ticket_id):
-    ticket_id = ticket_id.strip()
-    sql = """DELETE FROM tickets WHERE id ="""+ticket_id
-    cur = database_connection.cursor()
-    cur.execute(sql)
-    database_connection.commit()
-
-
-def print_table():
-    cur = database_connection.cursor()
-    cur.execute("SELECT * FROM tickets")
-    # Can be replaced with the property name I'm trying to get
-    tickets = cur.fetchall()
-    for ticket in tickets:
-        print(ticket)
-
-
-def print_number_of_active_tickets():
-    cur = database_connection.cursor()
-    cur.execute("SELECT COUNT(*) FROM tickets")
-    print(cur.fetchone()[0])
-
-
-def print_ticket(ticket_id):
-    ticket_id = ticket_id.strip()
-    cur = database_connection.cursor()
-    cur.execute("SELECT * FROM tickets WHERE id ="+ticket_id)
-    print(cur.fetchall())
+def validate_string_input(message):
+    string_input = input(message)
+    if len(string_input) and string_input != "":
+        return string_input.lower()
+    else:
+        print("Invalid input, ensure field isn't blank")
+        validate_string_input(message)
 
 
 # Press the green button in the gutter to run the script.
